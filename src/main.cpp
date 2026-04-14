3f9c024ef2be7022c85c55e175219fada093bd29
ocrambana <coccato.m@gmail.com>
2026-04-14 18:46:14 +0200

    Cover Image Rendering settings

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,64 @@ int main()
     // World
     hittable_list world;
 
-    auto mat_ground = make_shared<lambertian>(color(.8,.8,.0));
-    auto mat_center = make_shared<lambertian>(color(.1,.2,.5));
-    auto mat_right = make_shared<metal>(color(.8,.6,.2), .3);
-    auto mat_left = make_shared<dielectric>(1.5);
-    auto mat_bubble = make_shared<dielectric>(1./1.50);
-    
-    world.add(make_shared<sphere>(point3(     0, -100.5,    -1.0),  100, mat_ground));
-    world.add(make_shared<sphere>(point3(     0,      0,    -1.2),   .5, mat_center));
-    world.add(make_shared<sphere>(point3(  -1.0,      0,    -1.0),   .5, mat_left));
-    world.add(make_shared<sphere>(point3(  -1.0,      0,    -1.0),   .4, mat_bubble));
-    world.add(make_shared<sphere>(point3(   1.0,      0,    -1.0),   .5, mat_right));
+    auto grnd_mat = make_shared<lambertian>(color(.5,.5,.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, grnd_mat));
 
+    for (int a = -11; a < 11; a++)
+    {
+        for (int b = -11; b < 11; b++)
+        {
+            auto rng_mat = random_double();
+            point3 center{a + .9 * random_double(), .2, b + .9 * random_double()};
+
+            if((center - point3(4,.2,0)).length() > .9)
+            {
+                shared_ptr<material> sphere_mat;
+                if(rng_mat < .8) // diffuse
+                {
+                    color col = color::random() * color::random();
+                    sphere_mat = make_shared<lambertian>(col);
+                    world.add(make_shared<sphere>(center, .2, sphere_mat));
+                }
+                else if (rng_mat < .95) // metal
+                {
+                    color col = color::random(.5,1);
+                    double fuzz = random_double(0,.5);
+                    sphere_mat = make_shared<metal>(col, fuzz);
+                    world.add(make_shared<sphere>(center, .2, sphere_mat));
+                }
+                else // glass
+                {
+                    sphere_mat = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, .2, sphere_mat));
+                }
+            }
+        }
+    }
+    
+    auto mat1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0,1,0),1.0,mat1));
+    
+    auto mat2 = make_shared<lambertian>(color(.4,.2,.1));
+    world.add(make_shared<sphere>(point3(-4,1,0),1.0,mat2));
+    
+    auto mat3 = make_shared<metal>(color(.7,.6,.5),0.0);
+    world.add(make_shared<sphere>(point3(4,1,0),1.0,mat3));
+
+    
     camera cam{};
     cam.aspect_ratio = 16.0/9.0;
-    cam.img_width = 400;
-    cam.samples_per_pixels = 100;
+    cam.img_width = 1200;
+    cam.samples_per_pixels = 500;
     cam.max_depth = 50;
 
     cam.vfov = 20;
-    cam.lookfrom = point3(-2,2,1);
-    cam.lookat = point3(0,0,-1);
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat = point3(0,0,0);
     cam.vup = vec3(0,1,0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     cam.render(world);
